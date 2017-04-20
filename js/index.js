41f426ffedb7e5c034faba189ca5680cdc8e5e01
Xu yanru <xuyanru158@163.com>
2017-04-20 18:58:58 +0800

    change
    添加基本模块

--- js/index.js
+++ js/index.js
@@ -14,47 +14,68 @@ app.config(function ($routeProvider) {
 });  
 // CONTROLLERS ============================================
 // parent page controller 父控制器
-animateApp.controller('parentCtrl', function($scope,$location) {
-  
+app.controller('parentCtrl', function($scope,$location) {
+	$scope.animate=function(str){
+		$scope.noanimate=str;
+	}
+  $scope.animate(true);
+  $("body").css("height",$(window).height()+"px");
+  $("#viewOuter").css("height",$("body").height()+"px");
+  $("#viewOuter>div").css("height",$("body").height()+"px");
+  $("#viewOuter>div>div").css("height",$("body").height()+"px");
+  $("#viewOuter .page-inner").css("height",$("body").height()+"px");
 });
 
-// shopcart page controller 主页
-animateApp.controller('startCtrl', function($scope) {
+// start page controller 主页
+app.controller('startCtrl', function($scope) {
+	$("#start").css("height",$(window).height()+"px");
+	console.log($("#start").height());
  
 });
   
 // order page controller 点餐
-animateApp.controller('orderCtrl', function($scope) {
- 
+app.controller('orderCtrl', function($scope) {
+  $scope.animate(false);
 });
   
 // shopcart page controller 购物车
-animateApp.controller('shopcartCtrl', function($scope) {
- 
+app.controller('shopcartCtrl', function($scope) {
+  $scope.animate(false);
 });
 
 // check page controller 账单
-animateApp.controller('contactController', function($scope) {
- 
+app.controller('checkCtrl', function($scope) {
+   $scope.animate(false);
 });
 
 // underorder page controller 已下单
-animateApp.controller('underorderCtrl', function($scope) {
- 
+app.controller('underorderCtrl', function($scope) {
+   $scope.animate(false);
 });
 
 // bill page controller 结账
-animateApp.controller('billCtrl', function($scope) {
- 
+app.controller('billCtrl', function($scope) {
+   $scope.animate(false);
 });
 
 // myorder page controller 我的订单
-animateApp.controller('myorderCtrl', function($scope) {
- 
+app.controller('myorderCtrl', function($scope) {
+   $scope.animate(false);
 });
 
 // account page controller 我的账户
-animateApp.controller('accountCtrl', function($scope) {
- 
+app.controller('accountCtrl', function($scope) {
+   $scope.animate(false);
+});
+app.controller("menuCtrl",function($scope){
+	$scope.menuAnimate=function($event){
+		if($("#menu-left").hasClass("active")){
+			$("#menu-left").removeClass("active").addClass("close");
+			$("#viewOuter>div").removeClass("active").addClass("close");
+		}else{
+			$("#menu-left").removeClass("close").addClass("active");
+			$("#viewOuter>div").removeClass("close").addClass("active");
+		}
+	}
 });
 
